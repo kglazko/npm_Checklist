d152dd7599e2c7313d9b1100d05c0a848b1f524e
yglazko <yglazko@yglazko.local>
2015-06-30 14:29:21 -0700

    Added preliminary database integration

--- app.js
+++ app.js
@@ -6,6 +6,10 @@ var http = require('http');
 var express = require('express'),
     routes = require('./routes');
 
+//Nano Module Config
+var nano = require('nano')('http://localhost:5984');
+//Specify Database to use each time
+var db = nano.use('versions');
 // Configuration
 const PORT = 3000;
 const AUDIENCE = "http://localhost:" + PORT;
@@ -16,6 +20,8 @@ app.configure(function(){
   app.set('views', __dirname + '/views');
   app.set('view engine', 'jade');
   app.use(express.bodyParser());
+  app.use(express.json());
+  app.use(express.urlencoded());
   app.use(express.methodOverride());
   app.use(express.cookieParser());
   app.use(express.session({ secret: 'your secret here' }));
@@ -37,10 +43,34 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 app.post('/auth', routes.auth(AUDIENCE));
-app.post('/version_declare', routes.version_declare, function(req, res){
-  var version = req.body.ff_version;
-  console.log("Post Received: %s %s", version);
+//app.post('/version_declare', routes.version_declare, function(req, res){
+  //var version = req.body.ff_version;
+  //console.log("Post Received: %s %s", version);
+//});
+
+//Post a new version to the Database
+app.post('/version_create' ,function(req, resp){
+var version=req.body.ff_version;
+/*The second parameter phone is the id we are explicitly specifying*/
+db.insert({version:version,qa_sign:false},version,function(err, body) {
+if (err) {
+if(err.message === 'no_db_file') {
+           // create database and retry
+           return nano.db.create("versions", function () {
+            insert_version(req,res);
+           console.log("ARLOOO");
+            });
+          }
+console.log("Error creating version or version already exists");
+console.log (err.message);
+return;
+}
+console.log("Version was created sucessfully %s %s", version);
 });
+req.session.ver = req.body.ff_version;
+resp.redirect('/');
+}
+);
 app.get('/logout', routes.logout);
 
 var server = http.createServer(app);
--- routes/index.js
+++ routes/index.js
@@ -16,13 +16,37 @@ exports.index = function(req, resp){
 exports.version_declare = function(req, resp) {
   if (req.body.ff_version) {
     req.session.ver = req.body.ff_version;
+    //database(req, resp);
     console.log( "Setting version to: " + req.body.ff_version);
     return resp.redirect('/');
   }
 
   else {
     console.log("error error");
+    console.log(req.body.ff_version);
   }
+
+  function database(req, res){
+    var version=req.body.ff_version;
+/*The second parameter phone is the id we are explicitly specifying*/
+console.log("I made it here!!!");
+  db.insert({version:version,qa_sign:false},version,function(err, body) {
+    if (err) {
+  if(err.message === 'no_db_file') {
+            // create database and retry
+           return nano.db.create("versions", function () {
+            insert_version(req,res);
+            console.log("ARLOOO");
+          });
+         }
+  console.log("Error creating version or version already exists");
+console.log (err.message);
+return;
+}
+console.log("Version was created sucessfully %s %s", version);
+});
+
+}
 };
 
 exports.auth = function (audience) {
